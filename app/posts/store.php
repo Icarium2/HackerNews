4b81091180727202c7e5204c5098c6d9172bb944
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2021-01-12 00:54:47 +0100

    re-structured db again
    and finally managed to send a post to the db meeting all the requirements

--- app/posts/store.php
+++ app/posts/store.php
@@ -6,4 +6,41 @@ require __DIR__ . '/../autoload.php';
 
 // In this file we store/insert new posts in the database.
 
+
+
+if (!loggedIn()){
+    redirect('/');
+}
+
+
+if (isset(
+    $_POST['title'], 
+    $_POST['description'], 
+    $_POST['link'])) {
+    $title = trim(filter_var($_POST['title'], FILTER_SANITIZE_STRING));
+    $content = trim(filter_var($_POST['description'], FILTER_SANITIZE_SPECIAL_CHARS));
+    $url = trim(filter_var($_POST['link'], FILTER_SANITIZE_URL));
+    $usr = $_SESSION['user']['id'];
+    $date = date("Y-M-D H:i:s");
+
+    $stmnt = $pdo->prepare('INSERT INTO posts (user_id, content, headline, link, date) 
+    VALUES (:user_id, :content, :headline, :link, :date)');
+
+    if(!$stmnt) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $stmnt->execute([
+
+        ':user_id' => $usr,
+        ':content' => $content,
+        ':headline' => $title,
+        ':link' => $url,
+        ':date' => $date
+    ]);
+
+
+
+    }
+
 redirect('/');
--- newpost.php
+++ newpost.php
@@ -1,24 +1,52 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
+<?php
+if(!loggedIn()){
+    redirect('/login.php');
+}
+?>
+<h2>New Post</h2>
+
+<div class="createPostContainer">
+    <form action="/app/posts/store.php" 
+    method="post" 
+    enctype="multipart/form-data">
+
+        <label for="title">Title</label>
+        <br>
+        <input type="text" 
+        name="title" 
+        id="title" 
+        required />
+        <br>
+        <label for="description">Description</label>
+        <br>
+        <textarea id="description" 
+        name="description" 
+        placeholder="description" 
+        required>
+        </textarea>
+        <br>
+        <label for="link">Url</label>
+        <br>
+        <input type="link" 
+        name="link" 
+        id="link">
+        <br>
+        <br>
+        <button class="btn" type="submit">Create</button>
+
+    </form>
+</div>
 
 
 
 
 
 
-FORMS FOR NEW POSTS HERE ((FORMS))
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-<?php require __DIR__ . '/views/footer.php'; ?>
