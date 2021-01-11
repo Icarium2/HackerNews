42a2f3183ebfd631ea1a4bdf4c6c9f5e2feb2db3
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2021-01-11 16:51:37 +0100

    added editprofile + profile pages
    will sort out error-messages before redirecting if not logged in later

---
+++ editprofile.php
@@ -0,0 +1,9 @@
+<?php require __DIR__ . '/app/autoload.php'; ?>
+<?php require __DIR__ . '/views/header.php'; ?>
+
+
+
+EDIT PROFILE HERE FFS
+
+
+<?php require __DIR__ . '/views/footer.php'; ?>
--- profile.php
+++ profile.php
@@ -1,5 +1,12 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
+
+<?php if (!isset($_SESSION['user'])) : ?>
+       <?php redirect('/login.php'); ?>
+    <?php endif; ?>
+    
+
+
 <section class="profile">
     <div class="profileWrapper">
         <h1>Profile</h1>
@@ -12,7 +19,7 @@
         </div>
         </div>
     </div>
-
+<button class="editProfile"><a href="/editprofile.php">EDIT PROFILE</a></button>
 
 </section>
 
