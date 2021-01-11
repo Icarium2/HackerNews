2e82c1863d62a2a043ebdbb14917ddf02f20f1cd
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2021-01-11 13:44:16 +0100

    .

--- profile.php
+++ profile.php
@@ -1,8 +1,20 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
+<section class="profile">
+    <div class="profileWrapper">
+        <h1>Profile</h1>
+        <div class="profileImg">
+        <div class="bio">
+        
 
 
 
+        </div>
+        </div>
+    </div>
+
+
+</section>
 
 
 
