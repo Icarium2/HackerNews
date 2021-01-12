754d687483e3073070baa523f7fd6355fa2759ae
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2021-01-12 21:26:01 +0100

    Update header.php

--- views/header.php
+++ views/header.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title><?php echo $config['title']; ?></title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet">
