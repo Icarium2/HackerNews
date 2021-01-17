8457992f88606d63c9dcd3345eab511dfb539ade
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2021-01-17 15:10:03 +0100

    cleaning up lint

--- app/upvotes.php
+++ app/upvotes.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 require __DIR__ . '/autoload.php';
 
 //upvote on click - removes upvote if user has already upvoted the same post
-if  (loggedIn() && isset($_POST['post_id'])) 
-{
+if (loggedIn() && isset($_POST['post_id'])) {
     $stmnt = $pdo->prepare('SELECT upvote FROM upvotes WHERE post_id = :post_id AND user_id = :user_id');
     $usrID = $_SESSION['user']['id'];
     $postID = (int) $_POST['post_id'];
-    $upvoted = toggleUpvote($postID, $pdo); 
+    $upvoted = toggleUpvote($postID, $pdo);
     if ($upvoted) {
         $stmnt = $pdo->prepare('DELETE FROM upvotes WHERE user_id = :user_id AND post_id = :post_id');
-    } 
+    }
         else
     {
         $stmnt = $pdo->prepare('INSERT INTO upvotes (user_id, post_id)
+
     VALUES
+    
         (:user_id, :post_id)');
         
     }
@@ -25,8 +26,6 @@ if  (loggedIn() && isset($_POST['post_id']))
         ':user_id' => $usrID,
         ':post_id' => $postID
     ]);
-    
-    
 
 }
 redirect('/');
