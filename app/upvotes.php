70b9a38101f9c72a642b3dfadc55e8f9a2913339
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2021-01-17 15:15:08 +0100

    cleaning up lint errors

--- app/upvotes.php
+++ app/upvotes.php
@@ -13,17 +13,11 @@ if (loggedIn() && isset($_POST['post_id'])) {
     if ($upvoted) {
         $stmnt = $pdo->prepare('DELETE FROM upvotes WHERE user_id = :user_id AND post_id = :post_id');
     } else {
-        $stmnt = $pdo->prepare('INSERT INTO upvotes (user_id, post_id) VALUES (:user_id, :post_id)');    
+        $stmnt = $pdo->prepare('INSERT INTO upvotes (user_id, post_id) VALUES (:user_id, :post_id)');
     }
     $stmnt->execute([
         ':user_id' => $usrID,
         ':post_id' => $postID
     ]);
-
 }
 redirect('/');
-
-
-
-
-
